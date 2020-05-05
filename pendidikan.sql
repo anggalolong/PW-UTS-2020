3e79bb387e586eae7ecd264be75d4e6b1caa7e27
anggalolong <62576950+anggalolong@users.noreply.github.com>
2020-05-05 14:48:28 +0800

    Add files via upload

---
+++ pendidikan.sql
@@ -0,0 +1,30 @@
+<sql>
+<head>
+</table>
+<h2>Riwayat Pendidikan</h2>
+<ul>
+<li>Pendidikan Formal</li>
+<br/>
+<table border="1" cellpadding="1" cellspacing="1.5" width="450">
+<tr>
+<td><b>Tingkatan Pendidikan<b/></td>
+<td><b>LULUS</b></td>
+</tr>
+<tr>
+<td>SDN 3 lambara</td>
+<td>2003</td>
+</tr>
+<tr>
+<td>SMP negeri model terpadu madani</td>
+<td>2006</td>
+</tr>
+<tr>
+<td>SMA Negeri 1 palu</td>
+<td>2009</td>
+</tr>
+<tr>
+<td>Universitas merdeka malang</td>
+<td>2017</td>
+</tr>
+</table>
+<br/>
---
+++ pribadi.sql
@@ -0,0 +1,32 @@
+<sql>
+<head>
+</table>
+<h2>Identitas Pribadi</h2>
+<ul>
+<br/>
+<table border="1" cellpadding="1" cellspacing="1.5" width="450">
+<tr>
+<td><b>Nama<b/></td>
+<td><b>Angga Dandi Prasetya</b></td>
+</tr>
+<tr>
+<td>Tempat tanggal lahir</td>
+<td>palu, 26 juli 1998</td>
+</tr>
+<tr>
+<td>jenis kelamin</td>
+<td>laki laki</td>
+</tr>
+<tr>
+<td>Agama</td>
+<td>Islam</td>
+</tr>
+<tr>
+<td>Alamat</td>
+<td>Jalan trans sulawesi no.17</td>
+</tr>
+<td>Email</td>
+<td>lolongangga@gmail.com</td>
+</tr>
+</table>
+<br/>
